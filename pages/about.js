08b7081c191df84483e202ff6e0b4324a2510087
slx1996 <gurkanguler73@hotmail.com>
2022-04-11 19:45:57 +0300

    first commit

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+
+export default function About(){
+
+    return <div>
+        <h3>Selam</h3>
+        <Link href={"/"}>
+            Geri Dön
+        </Link>
+    </div>
+}
